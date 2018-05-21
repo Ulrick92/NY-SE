21677f05b7250e2ea5240d5dafe61d922c9cb32a
Baptiste Daniellou <b.daniellou@orange.fr>
2018-05-21 12:05:36 -0400

    Created socks routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :socks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
