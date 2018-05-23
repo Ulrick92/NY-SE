61253ab78c797ac1f661253810005b70aab9fe46
Anette Wahl <anewahl@gmail.com>
2018-05-23 12:23:31 +0200

    Removed a console log

--- app/javascript/components/slidebar.js
+++ app/javascript/components/slidebar.js
@@ -2,7 +2,6 @@ const slider = document.getElementById("myRange");
 const cleanliness = document.getElementById("sock_cleanliness");
 
 function sliderValue() {
-  // console.log(slider.value);
   cleanliness.value = slider.value;
 }
 
