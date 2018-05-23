33c6ea7e023e5851b0ac1f760a7b20619cbbd04d
Paul Henri Blaiset <paul.blaiset@gmail.com>
2018-05-23 15:07:42 +0200

    seed pb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-a = Sock.create!(
+Transaction.delete_all
+Sock.delete_all
+User.delete_all
+puts "Users and Socks and transactions deleted"
+
+
+michel = User.create!(
+  email: "michel@michel.com",
+  password: "password"
+  )
+
+
+puts 'user created'
+
+a = Sock.new(
   title: 'Nike noire',
   description: 'Nike noire gauche',
   size: 42,
@@ -13,11 +27,12 @@ a = Sock.create!(
   shipping_method: 'domicile',
   color: 'Noire',
   cleanliness: 5,
-  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526994888/thought-catalog-470985-unsplash.jpg',
-  user_id: 1
+  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526994888/thought-catalog-470985-unsplash.jpg'
 )
+a.user = michel
+a.save
 
-b = Sock.create!(
+b = Sock.new(
   title: 'Nike grise',
   description: 'Nike noire droite',
   size: 42,
@@ -25,11 +40,12 @@ b = Sock.create!(
   shipping_method: 'domicile',
   color: 'Noire',
   cleanliness: 5,
-  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526994889/yasin-hosgor-459740-unsplash.jpg',
-  user_id: 1
+  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526994889/yasin-hosgor-459740-unsplash.jpg'
 )
+b.user = michel
+b.save
 
-c = Sock.create!(
+c = Sock.new(
   title: 'Nike blanche',
   description: 'Nike blanche droite',
   size: 43,
@@ -37,11 +53,11 @@ c = Sock.create!(
   shipping_method: 'domicile',
   color: 'Blanche',
   cleanliness: 5,
-  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526999126/jstzei2rn7dl9itsir0i.jpg',
-  user_id: 1
+  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526999126/jstzei2rn7dl9itsir0i.jpg'
 )
+c.user = michel
 
-d = Sock.create!(
+d = Sock.new(
   title: 'Adidas blanche',
   description: 'Adidas blanche droite',
   size: 45,
@@ -49,12 +65,12 @@ d = Sock.create!(
   shipping_method: 'domicile',
   color: 'Blanche',
   cleanliness: 2,
-  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526999040/xpcbu3qdzdgtrd1ki6s3.jpg',
-  user_id: 1
+  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526999040/xpcbu3qdzdgtrd1ki6s3.jpg'
 )
+d.user = michel
+d.save
 
-
-e = Sock.create!(
+e = Sock.new(
   title: 'Puma blanche',
   description: 'Puma blanche droite',
   size: 41,
@@ -62,11 +78,12 @@ e = Sock.create!(
   shipping_method: 'domicile',
   color: 'Blanche',
   cleanliness: 7,
-  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526998981/e90b6dl3rbteoid74m0x.jpg',
-  user_id: 1
+  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526998981/e90b6dl3rbteoid74m0x.jpg'
 )
+e.user = michel
+e.save
 
-f = Sock.create!(
+f = Sock.new(
   title: 'Adidas Noire',
   description: 'Adidas noire droite',
   size: 43,
@@ -74,9 +91,10 @@ f = Sock.create!(
   shipping_method: 'domicile',
   color: 'noire',
   cleanliness: 10,
-  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526994888/thought-catalog-470985-unsplash.jpg',
-  user_id: 1
+  remote_photo_url: 'http://res.cloudinary.com/dp4vga3rz/image/upload/v1526994888/thought-catalog-470985-unsplash.jpg'
 )
+f.user = michel
+f.save
 
 puts 'created Socks'
 
