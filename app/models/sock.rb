5dcf5f241d48a3c5303670a52efb5345c3c78990
Paul Henri Blaiset <paul.blaiset@gmail.com>
2018-05-24 11:23:03 +0200

    ugly filter for color and size

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -3,10 +3,12 @@ class Sock < ApplicationRecord
   # has_one :transaction
   validates :title, presence: true
   validates :description, presence: true
-  validates :size, presence: true
+  SIZES = (35..47).to_a
+  validates :size, inclusion: { in: SIZES }
   validates :price, presence: true
   validates :shipping_method, presence: true
-  validates :color, presence: true
+  COLORS = %w(white black red blue green yellow purple orange pink gray marron)
+  validates :color, inclusion: { in: COLORS }
   validates :cleanliness, inclusion: { in: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }, numericality: {only_integer: true}
   mount_uploader :photo, PhotoUploader
   geocoded_by :location
